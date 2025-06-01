a8dc7cc46fbc210ee51176edce9113e9b197acac
Achiengmnoma <onyangostacya@gmail.com>
2025-06-01 19:04:59 +0100

    Functionality added to main/CancerServImpl file and other edits done to other files as well

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,6 +13,12 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello(){
+		return "hello world";
+	}
+
+
 //	@GetMapping
 //	public Cancerslist getCancerlist(){
 //		List<Cancers> cancers = List.of(new Cancers(
@@ -21,7 +28,8 @@ public class DemoApplication {
 //				"take exercises regularly"
 //				));
 //		return new Cancerslist("A",cancers);
-//	}THIS IS THE OFFICIAL ONE BEFORE CONTROLLER IS INTRODUCED
+//	}
+//	THIS IS THE OFFICIAL ONE BEFORE CONTROLLER IS INTRODUCED
 
 /*
  public List<Cancers> hey(){
---
+++ src/main/java/com/example/demo/service/CancersServ.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.io.Cancers;
+
+public interface CancersServ {
+    Cancers getName(String name);
+    Cancers getCauses(String causes);
+    Cancers getSymptoms(String symptoms);
+    Cancers getTreatment(String treatment);
+    Cancers getId(Long id);
+}
