a9e2e608b7363c58eb83d52e3470c8738a46beb0
Achiengmnoma <onyangostacya@gmail.com>
2025-10-27 21:56:49 +0000

    deleted the static list in the dto file(record file),did constructor injection in the controller file, and added a getAll() method for a no path entry that returns all entries.

--- src/main/java/com/example/demo/dto/Cancers.java
+++ src/main/java/com/example/demo/dto/Cancers.java
@@ -1,10 +1,5 @@
 package com.example.demo.dto;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public record Cancers(String name,String causes,String symptoms,String treatment,Long id) {
-    public static List<Cancers> cancerDisease = new ArrayList<>(Arrays.asList());
-
 }
